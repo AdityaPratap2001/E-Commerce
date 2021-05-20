2006d6633377bc8c6033c3da6f856422f54290e4
Aditya <pratapsinghaditya2001@gmail.com>
2021-05-20 12:49:08 +0530

    Removed E-Mail bug

--- Back-End/config/config.js
+++ Back-End/config/config.js
@@ -1,2 +1,3 @@
 exports.DB_KEY = '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';
 exports.TOKEN_KEY = 'RandomGibberish';
+exports.SEND_GRID_KEY = "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";
